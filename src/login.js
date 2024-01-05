e147c4b1949096f405e7d305fa6efaa60562a631
Mohamed Sharief <hello@moe-sharief.dev>
2024-01-05 20:28:48 +0300

    modified:   dist/anotherBundle.js 	modified:   src/login.js

--- src/login.js
+++ src/login.js
@@ -1,4 +1,18 @@
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDjoXCuxMjt9T4SiPJCeeYR5ZeEj06TlHs",
+  authDomain: "spacetravel-5de7a.firebaseapp.com",
+  projectId: "spacetravel-5de7a",
+  storageBucket: "spacetravel-5de7a.appspot.com",
+  messagingSenderId: "837488187883",
+  appId: "1:837488187883:web:55668ad1dc38157984e1ae"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 async function login(email, password) {
   try {
