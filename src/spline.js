ab9403ce55ad961f1aeb1f45e24e526f7182a5ac
Mohamed Sharief <hello@moe-sharief.dev>
2024-01-06 18:02:37 +0300

    modified:   .DS_Store 	modified:   assets/style.css 	modified:   index.html 	modified:   src/spline.js

--- src/spline.js
+++ src/spline.js
@@ -3,6 +3,9 @@ import { Application } from '@splinetool/runtime';
 // make sure you have a canvas in the body
 const canvas = document.getElementById('canvas3d');
 
+
+
 // start the application and load the scene
 const spline = new Application(canvas);
+
 spline.load('https://prod.spline.design/PVLBdf8lH7AY7QJx/scene.splinecode');
